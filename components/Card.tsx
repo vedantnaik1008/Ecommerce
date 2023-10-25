e8113fdb45b7a200fa1a35421b959c70bc366ce0
tallman <vedunaik@gmail.com>
2023-10-25 15:46:10 +0530

    added new img and height

--- components/Card.tsx
+++ components/Card.tsx
@@ -22,17 +22,17 @@ const Card = ({res} : Props) => {
       }
 
   return (
-    <div className="p-[20px] bg-white shadow-2xl border-none rounded-xl h-[100%]">
+    <div className="p-[20px] bg-white shadow-2xl border-none rounded-xl h-full">
           <Link href={`/details/${res._id}?id=${res._id}`} className="" key={res._id}>
             <div  className="flex items-center flex-col justify-center h-[60%]">
-              <Image src={res.image} alt="items-image" width={200} height={100} className="md:hover:scale-110 transition-all duration-300 ease-in-out flex overflow-hidden object-cover"/>
+              <Image src={res.image} alt="items-image" width={200} height={200} className="md:hover:scale-110 transition-all duration-300 ease-in-out flex overflow-hidden object-cover"/>
             </div>
             </Link>  
-            <div className="flex justify-between items-center mt-[20px]">
+            <div className="flex justify-between items-center mt-[20px] h-[10%]">
               <h2 className="text-black font-medium max-w-[200px]">{truncateText(res.title, 20)}</h2>
               <span className="bg-gray-300 px-2 py-1 border-none rounded-sm text-black font-bold relative z-20">${res.price}</span>
             </div>
-            <div className="flex justify-between items-center min-[320px]:my-[20px]  md:mt-[20px]">
+            <div className="flex justify-between items-center mt-[20px]  md:mt-[20px] h-[10%]">
               <button onClick={()=> dispatch(addItem(res))} className="bg-black rounded-[30px] text-white font-semibold py-1 px-4 hover:bg-slate-400">Add To Cart</button>
               <RatingStars rating={res.rating} />
             </div>
