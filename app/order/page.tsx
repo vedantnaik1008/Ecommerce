45111aca4a0bf8835b36fb687c1162e6d5d68cfe
tallman <vedunaik@gmail.com>
2023-11-18 17:07:14 +0530

    added coma

--- app/order/page.tsx
+++ app/order/page.tsx
@@ -7,7 +7,7 @@ import Button from '@/components/Button';
 import Link from 'next/link';
 
 const Order = async() => {
-    const order = await prisma.order.findMany()
+    const order = await prisma.order.findMany();
    
   return (
     <>
