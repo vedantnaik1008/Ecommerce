37399688094ae0c4675f29860c07ce3649f0a973
tallman <vedunaik@gmail.com>
2023-11-22 10:31:26 +0530

    added force dynamic

--- app/api/getOrder/route.ts
+++ app/api/getOrder/route.ts
@@ -1,6 +1,8 @@
 import { prisma } from "@/lib/db/prisma";
 import { NextResponse } from "next/server";
 
+export const dynamic = "force-dynamic";
+
 export async function GET(req: Request){
     try {
         const order = await prisma.order.findMany();
