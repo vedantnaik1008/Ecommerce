6ba8a5cafc239a4db41266af3423cb6be0bc3bc5
tallman <vedunaik@gmail.com>
2023-11-22 12:54:11 +0530

    +userId&id

--- app/api/order/route.ts
+++ app/api/order/route.ts
@@ -10,10 +10,12 @@ export async function POST(req: Request) {
   const orders = body
 
       for (const order of orders) {
-        const {title, category, description, image, price, rating, quantity} = order
+        const {id, userId, title, category, description, image, price, rating, quantity} = order
 
         await prisma.order.create({ 
           data: {
+            id,
+            userId,
             title,
             category,
             description,
