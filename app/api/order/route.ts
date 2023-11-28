d16845186520a30084bfae1950bd0fb9bda908e7
tallman <vedunaik@gmail.com>
2023-11-28 22:00:53 +0530

    added loop

--- app/api/order/route.ts
+++ app/api/order/route.ts
@@ -4,11 +4,20 @@ import { NextResponse } from "next/server";
 
 export async function POST(req: Request) {
   try {
-  
+    // const {id} = body
+
+    // const createOrder = await prisma.user.findUnique({
+    //   where: {
+    //     id
+    //   }
+    // })
+    // return NextResponse.json(createOrder)
   const body = await req.json()
 
-  
-        const {id} = body
+  const orders = body
+
+      for (const order of orders) {
+        const {id} = order
 
         const createOrder = await prisma.user.findUnique({
           where: {
@@ -16,8 +25,9 @@ export async function POST(req: Request) {
           }
         })
         return NextResponse.json(createOrder)
+      }
 
-        
+       
     
   } catch (error) {
     console.log("[ORDER_ERROR]", error);
