261b93393b9ca80913091f26780384525f01f3e8
tallman <vedunaik@gmail.com>
2023-11-28 22:04:48 +0530

    removed commented code

--- app/api/order/route.ts
+++ app/api/order/route.ts
@@ -4,14 +4,7 @@ import { NextResponse } from "next/server";
 
 export async function POST(req: Request) {
   try {
-    // const {id} = body
-
-    // const createOrder = await prisma.user.findUnique({
-    //   where: {
-    //     id
-    //   }
-    // })
-    // return NextResponse.json(createOrder)
+   
   const body = await req.json()
 
   const orders = body
