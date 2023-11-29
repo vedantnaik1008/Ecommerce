759c6a7f0ed6eed340603c03c2a6d520221fbbee
tallman <vedunaik@gmail.com>
2023-11-29 21:18:43 +0530

    changed schema and order/route file

--- app/api/order/route.ts
+++ app/api/order/route.ts
@@ -1,29 +1,49 @@
-import { prisma } from "@/lib/db/prisma";
-import { NextResponse } from "next/server";
-
+import { prisma } from '@/lib/db/prisma';
+import { auth, currentUser } from '@clerk/nextjs';
+import { NextResponse } from 'next/server';
 
 export async function POST(req: Request) {
-  try {
-   
-  const body = await req.json()
+    const body = await req.json();
+    const {
+        id,
+        userId,
+        title,
+        price,
+        description,
+        image,
+        rating,
+        category,
+        quantity,
+    } = body;
+    try {
+        const UserExist = await prisma.order.findFirst({
+            where: {
+                id: id,
+                userId,
+            },
+        });
 
-  const orders = body
+        if (!UserExist) {
+            return new NextResponse('no user found', { status: 401 });
+        }
 
-      for (const order of orders) {
-        const {id} = order
+        const createOrder = await prisma.order.create({
+            data: {
+                id,
+                userId,
+                title,
+                price,
+                description,
+                image,
+                rating,
+                category,
+                quantity,
+            },
+        });
 
-        const createOrder = await prisma.user.findUnique({
-          where: {
-            id
-          }
-        })
-        return NextResponse.json(createOrder)
-      }
-
-       
-    
-  } catch (error) {
-    console.log("[ORDER_ERROR]", error);
-    return new NextResponse("Internal Error", { status: 500 });
-  }
-};
+        return NextResponse.json(createOrder);
+    } catch (error) {
+        console.log('[ORDER_ERROR]', error);
+        return new NextResponse('Internal Error', { status: 500 });
+    }
+}
