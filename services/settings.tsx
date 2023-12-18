e41ed83151493e5c42fc18a06bc5ea86f08e3c5f
tallman <vedunaik@gmail.com>
2023-12-18 21:30:22 +0530

    +.color-white

--- services/settings.tsx
+++ services/settings.tsx
@@ -4,7 +4,7 @@ const NextArrow = (props: any) => {
     const { onClick } = props;
     return (
         <div
-            className='p-3 bg-slate-100 hover:text-orange-600 hover:bg-white cursor-pointer duration-200 rounded-full text-2xl flex items-center justify-center z-20 absolute   top-1/2 left'
+            className='p-3 color-white hover:text-orange-600 hover:bg-white cursor-pointer duration-200 rounded-full text-2xl flex items-center justify-center z-20 absolute   top left'
             onClick={onClick}>
             <PiCaretLeftLight />
         </div>
@@ -14,7 +14,7 @@ const PrevArrow = (props: any) => {
     const { onClick } = props;
     return (
         <div
-            className='p-3 bg-slate-100 hover:text-orange-600 hover:bg-white cursor-pointer duration-200 rounded-full text-2xl flex items-center justify-center z-20 absolute  right top-1/2'
+            className='p-3 color-white hover:text-orange-600 hover:bg-white cursor-pointer duration-200 rounded-full text-2xl flex items-center justify-center z-20 absolute  right top'
             onClick={onClick}>
             <PiCaretRightLight />
         </div>
