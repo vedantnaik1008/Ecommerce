803158bd05eb72e3fb7a2f1fbb31e08a0544f475
tallman <vedunaik@gmail.com>
2023-12-27 21:58:18 +0530

    removed useless code

--- components/ui/Header.tsx
+++ components/ui/Header.tsx
@@ -1,26 +1,11 @@
 'use client';
-import React, { useEffect } from 'react';
 import Nav from './Nav';
 import dynamic from 'next/dynamic';
 const Search = dynamic(() => import('./Search'), { ssr: false });
 
 const Header = () => {
-    useEffect(() => {
-        const onScroll = () => {
-            const scrollPosition = window.scrollY;
-            if (scrollPosition > 80) {
-                document.querySelector('header')?.classList.add('show');
-            } else {
-                document.querySelector('header')?.classList.remove('show');
-            }
-        };
-
-        window.addEventListener('scroll', onScroll);
-        return () => window.addEventListener('scroll', onScroll);
-    });
-
     return (
-        <header className='fixed z-50 w-full  py-[10px] sm:px-[10px] md:px-[20px] bg-white shadow-2xl '>
+        <header className='fixed z-50 w-full py-[10px] sm:px-[10px] md:px-[20px] bg-white shadow-2xl '>
             <div className='flex items-center justify-center'>
                 <div className='md:hidden w-[90%] mx-auto'>
                     <Search />
