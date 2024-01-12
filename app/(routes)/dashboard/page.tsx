abe0ace754582b722d4ac4db79090508a2d96053
tallman <vedunaik@gmail.com>
2024-01-12 11:10:03 +0530

    added dynamic loading

--- app/(routes)/dashboard/page.tsx
+++ app/(routes)/dashboard/page.tsx
@@ -1,8 +1,13 @@
 'use client';
-import SLiderCLothes from '@/components/SliderClothes';
-import SLiderElectronics from '@/components/SliderElectronics';
+const SLiderCLothes = dynamic(() => import('@/components/SliderClothes'), {
+    ssr: false
+});
+const SLiderElectronics = dynamic(() => import('@/components/SliderElectronics'),{
+        ssr: false
+});
 import Trending from '@/components/Trending';
 import Banner from '@/components/ui/Banner';
+import dynamic from 'next/dynamic';
 
 const Dashboard = () => {
     return (
