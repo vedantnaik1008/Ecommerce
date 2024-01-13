b935362ba3bf0c6f47e144a63d13afb1dcc96b19
tallman <vedunaik@gmail.com>
2024-01-13 11:35:13 +0530

    fixed card component height

--- components/ui/Card.tsx
+++ components/ui/Card.tsx
@@ -21,7 +21,7 @@ const Card = ({ res }: Props) => {
                 href={`/details/${res._id}?id=${res._id}`}
                 className=''
                 key={res._id}>
-                <div className='flex items-center flex-col justify-center h-[60%]'>
+                <div className='flex items-center flex-col justify-center h-[245px]'>
                     <Image
                         src={res.image}
                         sizes='200px'
@@ -32,7 +32,7 @@ const Card = ({ res }: Props) => {
                     />
                 </div>
             </Link>
-            <div className='flex justify-between items-center mt-[20px] h-[10%]'>
+            <div className='flex justify-between items-center mt-[20px]'>
                 <h2 className='text-black font-medium max-w-[200px]'>
                     {truncateText(res.title, 20)}
                 </h2>
@@ -40,7 +40,7 @@ const Card = ({ res }: Props) => {
                     ${res.price}
                 </span>
             </div>
-            <div className='flex justify-between items-center mt-[20px]  md:mt-[20px] h-[10%]'>
+            <div className='flex justify-between items-center my-[20px]  md:mt-[20px]'>
                 <button
                     onClick={() => dispatch(addItem(res))}
                     className='bg-black rounded-[30px] text-white font-semibold py-1 px-4 hover:bg-slate-400'>
