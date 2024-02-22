a8c241781a7ccc5411bdbf28e0aa17e6a8226a6d
tallman <vedunaik@gmail.com>
2024-02-22 15:26:42 +0530

    changed dashboard file location

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -2,15 +2,19 @@ import SLiderCLothes from '@/components/SliderClothes'
 import SLiderElectronics from '@/components/SliderElectronics';
 import Trending from '@/components/Trending';
 import Banner from '@/components/ui/Banner';
+import Footer from '@/components/ui/Footer';
+import Header from '@/components/ui/Header';
 
 const Dashboard = () => {
     return (
         <>
             <div className='bg-gray-300'>
+                <Header />
                 <Banner />
                 <Trending />
                 <SLiderCLothes />
                 <SLiderElectronics />
+                <Footer />
             </div>
         </>
     );
--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,4 @@
-import Dashboard from '@/app/(routes)/dashboard/page';
+import Dashboard from '@/app/dashboard/page';
 import { auth } from '@clerk/nextjs';
 import { redirect } from 'next/navigation';
 
